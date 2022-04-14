82d8e3715c807de8f850a0af9db77cf6ee3b53b6
becominginsane <francesco@zondax.ch>
2022-04-14 21:36:37 +0200

    fix(main): proper hashmap merge

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use git2::Repository;
 use git_heat::*;
 use time::OffsetDateTime;
 
+//TODO:
+// CLI: repository path
+//      date range
 fn main() {
     let repo = Repository::open(".").expect("not a valid git repo");
 
@@ -19,10 +22,14 @@ fn main() {
     let mut renames = HashMap::new();
     let changes = diffs.map(|diff| get_files_changed(diff, &mut renames));
 
-    let changes = changes.fold(HashMap::new(), |mut acc, changes| {
-        acc.extend(changes.into_iter());
-        acc
-    });
+    let changes = changes
+        .reduce(|mut acc, changes| {
+            for (k, v) in changes.into_iter() {
+                *acc.entry(k).or_default() += v;
+            }
+            acc
+        })
+        .unwrap_or_default();
 
     println!("{:?}", changes);
 }
